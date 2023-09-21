9b261a8f49faf7333b3420d282c0aaade8469aef
Anna Dávilla <110867038+AnnaDavilla@users.noreply.github.com>
2023-09-20 21:00:44 -0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,11 @@
+a = 33
+b = "33"
+
+if b> a:
+    print("b é maior que a")
+elif b == a:
+    print("a e b são iguais")
+
+#o resultado foi erro de sintaxe o parentese nao foi fechado
+#SyntaxError: '(' was never closed
+#TypeError: '>' not supported between instances of 'str' and 'int'
